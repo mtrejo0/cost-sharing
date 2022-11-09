a85e7345262f0887619a6ac971e0c27b835ff47e
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2022-11-09 10:28:10 -0500

    real build

--- src/config/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./firebase.config";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-export const Firebase = initializeApp(firebaseConfig);
-export const auth = getAuth();
-export const Providers = { google: new GoogleAuthProvider() };
-export const db = getFirestore(Firebase);
